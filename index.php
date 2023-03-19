b23d96fa933eb501d016a4f36cbb25c8a4b6f157
francois pepin <127262659+bigboss2711@users.noreply.github.com>
2023-03-19 02:53:46 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>my home page</title>
+        <phpinclude>
+        <commonmeta class="php"></commonmeta>
+        <phpinclude>
+        <commoncss class="php"></commoncss>
+        <phpinclude>
+        <theme class="php"></theme>
+        <phpinclude>
+        <commonscript class="php"></commonscript>
+    </head>
+    <body>
+        <phpinclude>
+        <navbar class="php"></navbar>
+<?php
+echo "Hello";
+?>
+</body>
+</html>
